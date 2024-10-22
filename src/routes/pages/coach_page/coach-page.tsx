f3d2183c9190791c2fedf21e662fe9ffa60e604d
Alex Hansen <hansen.alex@telia.com>
2024-10-22 23:42:45 +0200

    ai recommendation section done

--- src/routes/pages/coach_page/coach-page.tsx
+++ src/routes/pages/coach_page/coach-page.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import OpenAI from "openai";
 import "./coach-page.scss";
 
@@ -8,53 +8,42 @@ const openai = new OpenAI({
 });
 
 const queryOpenAI = async (query: string) => {
-  // const completion = await openai.chat.completions.create({
-  //   messages: [{ role: "system", content: query }],
-  //   model: "gpt-3.5-turbo",
-  //   // max_tokens: 100,
-  //   // max_tokens: 50,
-  //   max_tokens: 200,
-  // });
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: query }],
+    model: "gpt-3.5-turbo",
+    // max_tokens: 50,
+    // max_tokens: 100,
+    max_tokens: 200,
+  });
 
-  // return completion.choices[0].message.content;
-
-  return "Electric Guitar. Acoustic Guitar. Semi-Acoustic Guitar. Classical Guitar. Pick. Picks. Tuner. Capo. Metronome. Amplifier. Effect Pedal. Effect Pedals. Guitar Cable. Guitar Cable Adapter. Guitar Strap. Guitar Stand. Guitar Bag. Guitar Case.";
+  return completion.choices[0].message.content;
 };
 
-//-Chat Page:
-//how to make each exercise a little bit harder or easier (bpm)
-//beginner, intermediate, experienced songs / parts of songs
-//and put wikipedia hyper links to important terms, might need to import manually (ex. metronome)
-//maybe put links to ultimate guitar / other for songs & riffs
-
-// const experience = "beginner"; //beginner, intermediate, experienced
-// const amount = 1-5
-//I am a {experience} guitarist what...
-// console.log(
-//   await queryOpenAI(
-//     `What are ${amount} ${experience} guitarist exercises? Give me these in a short form.`
-//   )
-// );
-
-//click to expand
-// const exercise = "Practice basic chord transitions (e.g. C to G to D)";
-// console.log(
-//   await queryOpenAI(
-//     `Can you give me some pointers on the ${experience} guitarist exercises of: "${exercise}"? Give me this in a short form.`
-//   )
-// );
-
 type ExperienceLevels = "beginner" | "intermediate" | "experienced";
+type PractiseOptions = "exercise" | "riff" | "song";
 
 const CoachPage = () => {
   const [state, setState] = useState<"start" | "recommendation" | "chat">(
     "start"
   );
-  //Recommendation
-  const [amount, setAmount] = useState<number>(1);
+
   //Chat
   const [chatQuery, setChatQuery] = useState<string>("");
   const [chatMessages, setChatMessages] = useState<string[]>([]);
+
+  //Recommendation
+  const [experience, setExperience] = useState<ExperienceLevels>("beginner");
+  const [practiseOption, setPractiseOption] =
+    useState<PractiseOptions>("exercise");
+  const [amount, setAmount] = useState<number>(1);
+
+  const onExperienceChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setExperience(event.target.value as ExperienceLevels);
+  };
+
+  const onPracticeOptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPractiseOption(event.target.value as PractiseOptions);
+  };
 
   return (
     <main className="coach-page">
@@ -75,9 +64,30 @@ const CoachPage = () => {
         <>
           <h2>What are you looking for?</h2>
           <div className="recommendation">
-            <form action="">
-              <fieldset>
-                <h2>What is your experience level?</h2>
+            <form
+              onSubmit={async (event: FormEvent<HTMLFormElement>) => {
+                event.preventDefault();
+
+                const query = `Can you give me ${amount} ${experience} guitarist ${practiseOption}${
+                  amount > 1 ? "s" : ""
+                }?`;
+                const queryResponse = await queryOpenAI(query);
+
+                setChatMessages(() => [
+                  ...chatMessages,
+                  query,
+                  queryResponse || "Something went wrong, please ask again!",
+                ]);
+                setChatQuery("");
+                setState("chat");
+
+                setExperience("beginner");
+                setPractiseOption("exercise");
+                setAmount(1);
+              }}
+            >
+              <fieldset className="experience">
+                <h3>What is your experience level?</h3>
                 <span>
                   <label htmlFor="experience-beginner">Beginner</label>
                   <input
@@ -85,7 +95,8 @@ const CoachPage = () => {
                     name="experience"
                     id="experience-beginner"
                     value="beginner"
-                    defaultChecked
+                    onChange={onExperienceChange}
+                    checked={experience == "beginner"}
                   />
                 </span>
                 <span>
@@ -95,6 +106,8 @@ const CoachPage = () => {
                     name="experience"
                     id="experience-intermediate"
                     value="intermediate"
+                    onChange={onExperienceChange}
+                    checked={experience == "intermediate"}
                   />
                 </span>
                 <span>
@@ -104,11 +117,13 @@ const CoachPage = () => {
                     name="experience"
                     id="experience-experienced"
                     value="experienced"
+                    onChange={onExperienceChange}
+                    checked={experience == "experienced"}
                   />
                 </span>
               </fieldset>
-              <fieldset>
-                <h2>What?</h2>
+              <fieldset className="practise">
+                <h3>What do you want to practise?</h3>
                 <span>
                   <label htmlFor="what-exercise">Exercise</label>
                   <input
@@ -116,33 +131,50 @@ const CoachPage = () => {
                     name="what"
                     id="what-exercise"
                     value="exercise"
-                    defaultChecked
+                    onChange={onPracticeOptionChange}
+                    checked={practiseOption == "exercise"}
                   />
                 </span>
                 <span>
-                  <label htmlFor="what-song">Riff</label>
-                  <input type="radio" name="what" id="what-riff" value="riff" />
+                  <label htmlFor="what-riff">Riff</label>
+                  <input
+                    type="radio"
+                    name="what"
+                    id="what-riff"
+                    value="riff"
+                    onChange={onPracticeOptionChange}
+                    checked={practiseOption == "riff"}
+                  />
                 </span>
                 <span>
                   <label htmlFor="what-song">Song</label>
-                  <input type="radio" name="what" id="what-song" value="song" />
+                  <input
+                    type="radio"
+                    name="what"
+                    id="what-song"
+                    value="song"
+                    onChange={onPracticeOptionChange}
+                    checked={practiseOption == "song"}
+                  />
                 </span>
               </fieldset>
-              <fieldset>
-                <h2>How many?</h2>
-                <h3>{amount}</h3>
-                <input
-                  type="range"
-                  name=""
-                  id=""
-                  min={1}
-                  max={5}
-                  step={1}
-                  value={amount}
-                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                    setAmount(Number(event.currentTarget.value) || 1)
-                  }
-                />
+              <fieldset className="amount">
+                <h3>How many?</h3>
+                <span>
+                  <input
+                    type="range"
+                    name=""
+                    id=""
+                    min={1}
+                    max={5}
+                    step={1}
+                    value={amount}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                      setAmount(Number(event.currentTarget.value) || 1)
+                    }
+                  />
+                  <h4>{amount}</h4>
+                </span>
               </fieldset>
               <button type="submit">Submit</button>
             </form>
@@ -162,7 +194,7 @@ const CoachPage = () => {
                   message: string
                 ): (string | JSX.Element)[] => {
                   const wordListRegex =
-                    /(electric guitar|acoustic guitar|semi-acoustic guitar|classical guitar|pick|tuner|capo|metronome|amplifier|effect pedal|guitar cable adapter|guitar cable|guitar strap|guitar stand|guitar bag|guitar case)/gi;
+                    /(electric guitar|acoustic guitar|semi-acoustic guitar|classical guitar|pick\b|tuner|capo|metronome|amplifier|effect pedal|guitar cable adapter|guitar cable|guitar strap|guitar stand|guitar bag|guitar case)/gi;
 
                   return message
                     .split(wordListRegex)
@@ -211,13 +243,13 @@ const CoachPage = () => {
               <button
                 type="button"
                 onClick={async () => {
-                  if (chatQuery.length < 1 /* || loading */) return;
+                  if (chatQuery.length < 1) return;
                   const queryResponse = await queryOpenAI(chatQuery);
 
                   setChatMessages(() => [
                     ...chatMessages,
                     chatQuery,
-                    queryResponse,
+                    queryResponse || "Something went wrong, please ask again!",
                   ]);
                   setChatQuery("");
                 }}
